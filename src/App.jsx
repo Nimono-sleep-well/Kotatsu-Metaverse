c777569a90a9bf10ea2657ab813e8a4106ace6ff
Nimono-sleep-well <monjitto.1119@outlook.jp>
2024-12-16 01:40:31 +0900

    ReactRouterによるページ遷移

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import {BrowserRouter, Route, Routes, Link} from "react-router-dom"
+
+import Home from './views/Home'
+import Signin from './views/Signin'
+import SelectRoom from './views/SelectRoom'
+import NotFound from './views/NotFound'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1>hoge</h1>
+      <div className='App'>
+        <BrowserRouter>
+          <Link to="/">Signin</Link> | <Link to="/Home">Home</Link> | <Link to="/SelectRoom">SelectRoom</Link>
+          <Routes>
+            <Route path='/' element={<Signin name="asssdaf" age="10"/>}></Route>
+            <Route path='/Home' element={<Home/>}></Route>
+            <Route path='/SelectRoom' element={<SelectRoom/>}></Route>
+            <Route path='*' element={<NotFound/>}></Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   )
---
+++ src/views/Signin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Signin = ({ name, age }) => {
+    return (
+        <div>
+            <h1>{age}</h1>
+            <h2>{name}</h2>
+        </div>
+    )
+}
+
+export default Signin
