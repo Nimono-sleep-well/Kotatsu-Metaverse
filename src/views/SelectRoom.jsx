6ef2eaa385faec211bca389934a4a009b640e37d
Nimono-sleep-well <monjitto.1119@outlook.jp>
2024-12-17 03:35:24 +0900

    complete system of create and delete room

--- src/views/SelectRoom.jsx
+++ src/views/SelectRoom.jsx
@@ -1,4 +1,4 @@
-import { collection, getDocs, onSnapshot, orderBy, query, QuerySnapshot } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, getDocs, onSnapshot, orderBy, query, QuerySnapshot } from "firebase/firestore";
 import { db } from "../../firebaseConfig"
 import { React, useState, useEffect } from "react"
 
@@ -19,16 +19,47 @@ const SelectRoom = () => {
         return unsub;
     }, []);
 
+    const createRoom = async (event) => {
+        event.preventDefault();
+        const { name } = event.target.elements;
+        const roomsCollectionRef = collection(db, 'rooms');
+        const documentRef = await addDoc(roomsCollectionRef, {
+            name: name.value,
+            peopleInRoom: 0,
+        });
+    };
+
+    const deleteRoom = async (id) => {
+        const roomsDocumentRef = doc(db, 'rooms', id);
+        await deleteDoc(roomsDocumentRef);
+    }
+
     return (
         <div>
             <h1>RoomList</h1>
             {rooms.map((room) => (
                 <div key={room.id}>
-                    <span>{room.id}</span>
-                    <span> | </span>
-                    <span>{room.peopleInRoom}</span>
+                    <span>
+                        <button className="bg-red-50">
+                            <span>{room.name}</span>
+                            <span> | </span>
+                            <span>{room.peopleInRoom}</span>
+                        </button>
+                    </span>
+                    <span>
+                        <span> | </span>
+                        <button onClick={() => deleteRoom(room.id)}>Delete</button>
+                    </span>
                 </div>
             ))}
+            <form onSubmit={createRoom}>
+                <span>
+                    <input name="name" type="text" placeholder="Roomの名前" />
+                </span>
+                <span>
+                    <button>Create</button>
+                </span>
+            </form>
         </div>
     )
 }
