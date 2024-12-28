ef5663695befe7b992c4ef974f360f4b1c07e46a
moritadaisuki <haru0421@icloud.com>
2024-12-28 18:04:21 +0900

    image switching

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Clock from '../components/Clock'
 import Chat from '../components/Chat'
+import Kotatsu from '../components/Kotatsu'
 
 const Home = ({id}) => {
     return (
         <div>
             <h1>{id}</h1>
             <Clock></Clock>
+            <Kotatsu></Kotatsu>
             <Chat roomID={id}></Chat>
         </div>
     )
