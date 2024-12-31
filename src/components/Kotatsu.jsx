7b006b6f326fdf30a27fb422c9a65ea42bc6c560
moritadaisuki <haru0421@icloud.com>
2025-01-01 00:54:14 +0900

    assign a direction and limiting the number of people

--- src/components/Kotatsu.jsx
+++ src/components/Kotatsu.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React ,{useState}from 'react'
 const Kotatsu = () => {
-  const east = false;
-  const west = true; 
+  const [east,setEast]= useState(false);
+  const [west,setWest]= useState(true); 
+  const [north,setNorth]= useState(false);
+  const peopleInKotatsu=1;
   let imageSrc = '';
 
     if(east&&west){
-      imageSrc='/kotatsu.png';
+      imageSrc='/kotatu Free image.jpg';
     }else if(east){
       imageSrc='/kotatsu.png';
     }else if(west){
@@ -14,9 +16,31 @@ const Kotatsu = () => {
       imageSrc='/kotatsu.png';
     }
 
+    const handleClick = () => {
+      if (!east) {
+        setEast(true);
+      } else if (!west) {
+        setWest(true);
+      } else if (!north) {
+        setNorth(true);
+      } else {
+        return(
+          <div>
+            <p>こたつに３人いるので入れません</p>
+          </div>
+        )
+      }
+    }
+
   return (
     <div>
-      <img src={imageSrc} alt="Location" />
+      <img src={imageSrc} alt="Location" onClick={handleClick}/>
+      <p>現在のこたつの状態:</p>
+      <ul>
+        <li>East: {east ? 'Occupied' : 'Empty'}</li>
+        <li>West: {west ? 'Occupied' : 'Empty'}</li>
+        <li>North: {north ? 'Occupied' : 'Empty'}</li>
+      </ul>
     </div>
   )
 }
