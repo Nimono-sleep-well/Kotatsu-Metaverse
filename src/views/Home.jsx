d1bf303535a0d106d7223e6643246ddec290009d
Nimono-sleep-well <monjitto.1119@outlook.jp>
2025-01-04 03:13:40 +0900

    部屋を壁と床の要素に分ける

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import Clock from '../components/Clock'
 import Chat from '../components/Chat'
 import Kotatsu from '../components/Kotatsu'
+import User from '../components/User'
 
 const Home = ({id}) => {
+
+    const x = 0;
+    const y = 100;
+
     return (
-        <div>
-            <h1>{id}</h1>
-            <Clock></Clock>
-            <Kotatsu></Kotatsu>
-            <Chat roomID={id}></Chat>
+        <div className='w-screen h-screen bg-blue-400 flex flex-col items-center justify-center'>
+            <div className='bg-red-300 w-full h-[91%]'> 
+                <img className='w-full h-full' src="/thumbnailSample.png" alt="room" />
+            </div>
+            <div className='flex bg-yellow-950 w-full h-[9%]'>
+                
+            </div>
         </div>
     )
 }
