9fbfa9ec7221e69c68a66ee51b32f0f54d61637c
Nimono-sleep-well <monjitto.1119@outlook.jp>
2025-01-04 13:32:34 +0900

    Homeにこたつ追加

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,18 +4,20 @@ import Chat from '../components/Chat'
 import Kotatsu from '../components/Kotatsu'
 import User from '../components/User'
 
-const Home = ({id}) => {
+const Home = ({ id }) => {
 
     const x = 0;
     const y = 100;
 
     return (
         <div className='w-screen h-screen bg-blue-400 flex flex-col items-center justify-center'>
-            <div className='bg-red-300 w-full h-[91%]'> 
+            <div className='bg-red-300 w-full h-[91%]'>
                 <img className='w-full h-full' src="/thumbnailSample.png" alt="room" />
             </div>
-            <div className='flex bg-yellow-950 w-full h-[9%]'>
-                
+            <div className='flex bg-yellow-950 w-full h-[9%] items-center justify-center'>
+                <div className='-translate-y-52 scale-125'>
+                    <Kotatsu></Kotatsu>
+                </div>
             </div>
         </div>
     )
